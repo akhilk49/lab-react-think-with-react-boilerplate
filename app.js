8159874d10ac3bdbf29b202ae694a5dc47a062e6
akhilk49 <akhilkkurian49@gmail.com>
2024-02-05 11:17:45 +0530

    okk

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-const container = document.getElementById("react-container");
+const containers=document.getElementById("react")
 
-ReactDOM.render("Heeyy Everyone...",container);
-
+ReactDOM.render("Hello welcome guys",containers)
 
 // Functional component
 const Container = () =>{
-  return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
-     React.createElement(`div`,null,`Let's rock and roll`)
-     );
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
 }
 // Render the Container
-const container = document.getElementById('react-container');
-ReactDOM.render(React.createElement(Container),container);
-
-
+const containe = document.getElementById('re');
+ReactDOM.render(React.createElement(Container),containe);
 
 
 class ReactContainer extends React.Component{
-  // constructor - to initialize the state 
-
-  // render method to render the react dom 
-    render(){
-        return React.createElement(`div`,null,`Hey Kalvians`,
-        React.createElement(`div`,null,`Let's rock and roll with classes`)
-        );
-    }
+    // constructor - to initialize the state 
   
-}
-const container = document.getElementById('react-container');
-ReactDOM.render(React.createElement(ReactContainer),container);
+    // render method to render the react dom 
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+  const con = document.getElementById('con');
+  ReactDOM.render(React.createElement(ReactContainer),con);
 
 
 // class ReactContainer extends React.Component{
